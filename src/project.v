2577163029a63fb4b83184b8f33afdfa37094e37
Isabel Walth <iwalth@ucsc.edu>
2026-03-03 12:01:13 -0800

    testbench

--- src/project.v
+++ src/project.v
@@ -15,23 +15,29 @@ module tt_um_ALU (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+
+//operand a 5-bit, operand b 5-bit, operation op 2-bit
   wire [4:0] a, b;
   wire [1:0] op;
   assign a = ui_in[4:0];
   assign b = uio_in[4:0];
   assign op = ui_in[7:6];
 
+//calculate values using a and b, store
   wire [7:0] add, sub, And, Or;
   assign add = a + b;
   assign sub = a - b;
   assign And = a & b;
   assign Or  = a | b;
 
+//mux, use first op bit to decide which operation is being used, (00, 01, 10, 11)
   wire [7:0] mux1_out, mux2_out;
 
+//load = 1 -> sub/OR, load = 0 -> add/AND
   mux8bit mux1 (.a_i(add), .b_i(sub), .ld_i(op[0]), .y_o(mux1_out));
   mux8bit mux2 (.a_i(And), .b_i(Or), .ld_i(op[0]), .y_o(mux2_out));
 
+//load = 1 -> AND or OR, load = 0 -> add or sub
   wire [7:0] mux3_o;
   assign uo_out = mux3_o;
   mux8bit mux3 (.a_i(mux1_out), .b_i(mux2_out), .ld_i(op[1]), .y_o(mux3_o));
@@ -45,6 +51,7 @@ module tt_um_ALU (
 
 endmodule
 
+//2 to 1 mux
 module mux8bit(
     input  [7:0] a_i,
     input  [7:0] b_i,
